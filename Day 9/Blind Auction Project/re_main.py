dc7fa974679d5a7f0e2369fb334ca5dfa270fd16
ivan.radosavljevic <ivan.radosavljevic@kupujemprodajem.com>
2025-09-26 15:05:37 +0200

    Refreshing the dictionary

---
+++ Day 9/Blind Auction Project/re_main.py
@@ -0,0 +1,29 @@
+#TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+import art
+bids = {}
+
+game_is_on = True
+print(art.logo)
+print("Welcome to the secret auction program.")
+
+while game_is_on:
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    bids[name] = bid
+    to_continue = input("Are there any other bidders? 'yes' or 'no': ")
+    if to_continue == "no":
+        game_is_on = False
+    else:
+        print("\n"*20)
+
+winner_name = ""
+max_bet = 0
+for key in bids:
+    if bids[key] > max_bet:
+        winner_name = key
+        max_bet = bids[key]
+
+print(f"{winner_name} is the winning bidder, with the bid of {max_bet}.")
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,14 +1,43 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+# programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+#
+# print(programming_dictionary["Bug"])
+#
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
+#
+#
+# programming_dictionary["Bug"] = "A moth in your computer."
+#
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
 
-print(programming_dictionary["Bug"])
 
-programming_dictionary["Loop"] = "The action of doing something over and over again."
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
 
+# travel_log = {
+#     "France": ["Paris", "Nice", "Grenoble"],
+#     "Germany": ["Berlin", "Stuttgart"],
+# }
 
-programming_dictionary["Bug"] = "A moth in your computer."
+# print(travel_log["France"][1])
 
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+nested_list = ["A", "B", ["C", "D"]]
 
+# print(nested_list[2][1])
+
+travel_log = {
+    "France": {
+        "times_visited": 1,
+        "cities_visited": ["Paris", "Nice", "Grenoble"],
+    },
+    "Germany": {
+        "times_visited": 7,
+        "cities_visited": ["Berlin", "Stuttgart"],
+    },
+}
+
+print(travel_log["Germany"]["cities_visited"][1])
 
